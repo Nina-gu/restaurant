1d505c72462336ac469ad7abe814fed143cdd260
Nina-gu <lotusgu9@gmail.com>
2021-02-23 23:02:41 -0600

    add footer, header, and their css style files.

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2021 Restaurant List All Rights Reserved. Website Made by Hannah
+            </footer>
+        );
+    }
+}
+
+export default Footer;
