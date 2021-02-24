42a8b5384af08e75227e038891bf78f727a6cebf
Nina-gu <lotusgu9@gmail.com>
2021-02-23 21:00:18 -0600

    statesList.js and genresList.js; The two list help later filter function

---
+++ src/components/constants/genresList.js
@@ -0,0 +1,9 @@
+exports.GENRES = [
+    { id: 1, name: 'american' },
+    { id: 2, name: 'asian' },
+    { id: 3, name: 'bakery' },
+    { id: 4, name: 'cafe' },
+    { id: 5, name: 'french' },
+    { id: 6, name: 'grill' },
+    { id: 7, name: 'seafood' },
+];
